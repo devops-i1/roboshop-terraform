817e8c6ba15b2780b4ec5a38c01e3b604cec8c8b
Akhil <akhilmahendra035@gmail.com>
2024-12-20 16:00:55 +0530

    roboshop terraform

--- main.tf
+++ main.tf
@@ -21,3 +21,17 @@ resource "aws_route53_record" "record" {
   records = [aws_instance.instance[count.index].private_ip]
   ttl     = 3
 }
+
+module "vpc" {
+  source = "./modules/vpc"
+  availability_zones     = var.availability_zones
+  backend_subnets        = var.backend_subnets
+  db_subnets             = var.db_subnets
+  default_route_table_id = var.default_route_table_id
+  default_vpc_cidr       = var.default_vpc_cidr
+  default_vpc_id         = var.default_vpc_id
+  env                    = var.env
+  frontend_subnets       = var.frontend_subnets
+  public_subnets         = var.public_subnets
+  vpc_cidr_block         = var.vpc_cidr_block
+}
