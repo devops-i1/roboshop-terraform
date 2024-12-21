dfdfab9e834baa8f242767138e8b2a78dc6758a3
Akhil <akhilmahendra035@gmail.com>
2024-12-21 18:46:16 +0530

    Ec2 in backend subnets

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "instance" {
   count         = length(var.components)
   ami           = "ami-07fef30b81ec93e2e"
   instance_type = "t3.small"
-  vpc_security_group_ids = ["sg-050c53e3ec8b8a873"]
+  vpc_security_group_ids = module.vpc.backend_subnets[0]
 
   tags = {
     Name = var.components[count.index]
