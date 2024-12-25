6e5cdcc8a17d28a97235881dfe43242a697691a6
Akhil <akhilmahendra035@gmail.com>
2024-12-25 13:03:43 +0530

    Add Route53 permission

--- modules/eks/iam.tf
+++ modules/eks/iam.tf
@@ -60,3 +60,8 @@ resource "aws_iam_role_policy_attachment" "node-AmazonEC2ContainerRegistryReadOn
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
   role       = aws_iam_role.node-role.name
 }
+
+resource "aws_iam_role_policy_attachment" "node-AmazonRoute53AutoNamingFullAccess" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonRoute53AutoNamingFullAccess"
+  role       = aws_iam_role.node-role.name
+}
